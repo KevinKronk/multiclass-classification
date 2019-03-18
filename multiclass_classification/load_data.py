91d9a1d8ad9874ed64e418ab78aa2a44385a0de2
Kevin Kronk <kjkronk@ncsu.edu>
2019-03-18 10:52:27 -0400

    Sigmoid Module.

--- multiclass_classification/load_data.py
+++ multiclass_classification/load_data.py
@@ -3,7 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
-filename = "multiclass-classification/ex3data1.mat"
+filename = "ex3data1.mat"
 
 data = loadmat(filename)
 print(data)
+print(data["X"].shape, data["y"].shape)
+
+
