829af6b4f9605c28b931de8e8525f4c75d36e73e
Kevin Kronk <kjkronk@ncsu.edu>
2019-03-19 15:07:01 -0400

    Added visualization.

--- multiclass_classification/classification.py
+++ multiclass_classification/classification.py
@@ -2,10 +2,31 @@ from load_data import load_data
 import numpy as np
 from multiclass import multiclass
 from predict_class import predict_class
+import matplotlib.pyplot as plt
 
 filename = "ex3data1.mat"
 
 x, y = load_data(filename)
+
+# --------------------------
+nrows = 15
+
+
+def row_to_square(x_row):
+    return np.reshape(x_row, (20, 20)).T
+
+
+# Choose nrows random rows of X.
+indices = np.random.randint(x.shape[0], size=nrows)
+X_sel = x[indices, :]
+
+fig, ax = plt.subplots(1, nrows, figsize=(20, 20))
+
+for i in range(nrows):
+    ax[i].imshow(row_to_square(X_sel[i, :]), cmap='gray')
+    ax[i].axis('off')
+plt.show()
+# --------------------------------------
 
 hyper_p = 0.1
 k = np.unique(y).size
